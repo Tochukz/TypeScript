57ca387a1562c11e05d5e45956e2a9163b3c4fd4
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-09-27 05:59:15 +0200

    Typescript Basics

--- TypeScriptEssentials/ts/greeting.ts
+++ TypeScriptEssentials/ts/greeting.ts
@@ -10,7 +10,8 @@ interface Person {
 }
 function greetPerson(person: Person){
     return "Hello, "+person.firstName + ' ' + person.lastName;
+    //Best to use a class instead for type hinting since the interface will be removes at compile time. 
 }
-let person = {firstName:'Jonny', lastName:'Bravo'}; 
+let person = {firstName:'Jonny', lastName:'Bravo'};
 //This implemenets the Person interface without using the 'implements' keyword, i call it implicit implementation
 document.body.innerHTML = greetPerson(person);
